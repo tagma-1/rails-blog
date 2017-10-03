3a2b6df6a890f407d8454691de4b66d1a7d7e2c8
Tagma 1 <31788490+tagma-1@users.noreply.github.com>
2017-10-03 01:36:46 +0000

    Added delete function and form partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,6 +36,13 @@ class ArticlesController < ApplicationController
     end
   end
   
+  def delete
+    @article = article.find(params[:id])
+    @article.destroy
+    
+    redirect_to articles_path
+  end
+  
   private
   
     def article_params
