3ffc68086761d0efb7df10229a0b3b51bb2fc80f
Tagma 1 <31788490+tagma-1@users.noreply.github.com>
2017-10-06 05:07:52 +0000

    Added comment form and comment display to article pages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,14 @@
 class WelcomeController < ApplicationController
-  def index
+  def create
+    @article = Article.find(params[:id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
   end
+  
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
+
 end
